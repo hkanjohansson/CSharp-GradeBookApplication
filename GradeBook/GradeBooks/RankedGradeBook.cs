20d236c167d2dd31f4175449885bc98cb329cbc4
hkan <hj222je@student.lnu.se>
2022-03-31 15:43:51 +0200

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,34 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char grade = 'F';
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of five students to work");
+            }
+
+            if (averageGrade >= 0.9)
+            {
+                grade = 'A';
+            }
+            else if (averageGrade >= 0.8)
+            {
+                grade = 'B';
+            }
+            else if (averageGrade >= 0.7)
+            {
+                grade = 'C';
+            }
+            else if (averageGrade >= 0.6)
+            {
+                grade = 'D';
+            }
+                
+            
+            return grade;
+        }
     }
 }
